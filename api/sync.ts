15154c654330c22223216d5218d068e5cb2528f5
ZHAO Xudong <zxdong@gmail.com>
2023-07-07 09:37:54 +0800

    support test api

--- api/sync.ts
+++ api/sync.ts
@@ -6,7 +6,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   const {
     method = ''
   } = req
-  const arr = ['GET', 'PUT']
+  const arr = ['GET', 'PUT', 'POST']
   if (!arr.includes(method)) {
     return res.status(404).send('404 not found')
   }
@@ -14,6 +14,8 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (typeof id !== 'string') {
     return res.status(401).send(id)
   }
-  const r = await getData(id, method === 'GET' ? undefined : req.body)
+  let r = method === 'POST'
+    ? 'test ok'
+    : await getData(id, method === 'GET' ? undefined : req.body)
   res.status(r ? 200 : 404).send(r || '404')
 }
